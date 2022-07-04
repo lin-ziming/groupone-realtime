1a893877ff7b0492e45f2db54c122308f0609bd2
chris <xychen213@qq.com>
2022-07-04 16:55:26 +0800

    log数据分流：BaseLogApp 导入DateFormatUtil FlinkSinkUtil：更新getKafkaSink()

--- edu/src/main/java/com/atguigu/common/Constant.java
+++ edu/src/main/java/com/atguigu/common/Constant.java
@@ -1,13 +1,20 @@
 package com.atguigu.common;
 
 public class Constant {
-    public static final String KAFKA_BROKERS = "hadoop302:9092,hadoop303:9092,hadoop304:9092";
-    public static final String PHOENIX_DRIVER = "org.apache.phoenix.jdbc.PhoenixDriver";
-    public static final String PHOENIX_URL = "jdbc:phoenix:hadoop302,hadoop303,hadoop304:2181";
-    public static final String TOPIC_ODS_DB = "ods_db";
-    public static final String TOPIC_ODS_LOG = "ods_log";
+	public static final String KAFKA_BROKERS = "hadoop302:9092,hadoop303:9092,hadoop304:9092";
+	public static final String PHOENIX_DRIVER = "org.apache.phoenix.jdbc.PhoenixDriver";
+	public static final String PHOENIX_URL = "jdbc:phoenix:hadoop302,hadoop303,hadoop304:2181";
+	public static final String TOPIC_ODS_DB = "ods_db";
+	public static final String TOPIC_ODS_LOG = "ods_log";
 
-    public static final String CLICKHOUSE_DRIVER = "ru.yandex.clickhouse.ClickHouseDriver";
-    public static final String CLICKHOUSE_URL = "jdbc:clickhouse://hadoop302:8123/gmall2022";
-    public static final int REDIS_DIM_TTL = 2 * 24 * 60 * 60;  //维度ttl:2天
+	public static final String CLICKHOUSE_DRIVER = "ru.yandex.clickhouse.ClickHouseDriver";
+	public static final String CLICKHOUSE_URL = "jdbc:clickhouse://hadoop302:8123/gmall2022";
+	public static final int REDIS_DIM_TTL = 2 * 24 * 60 * 60;  //维度ttl:2天
+
+	public static final String TOPIC_DWD_TRAFFIC_PAGE = "dwd_traffic_page";
+    public static final String TOPIC_DWD_TRAFFIC_START = "dwd_traffic_start";
+    public static final String TOPIC_DWD_TRAFFIC_ERR = "dwd_traffic_err";
+    public static final String TOPIC_DWD_TRAFFIC_DISPLAY = "dwd_traffic_display";
+    public static final String TOPIC_DWD_TRAFFIC_ACTION = "dwd_traffic_action";
+    public static final String TOPIC_DWD_TRAFFIC_APPVIDEO = "dwd_traffic_appvideo";
 }
