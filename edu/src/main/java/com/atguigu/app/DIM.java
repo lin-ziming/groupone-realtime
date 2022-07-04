f52fd830910de19a3b44521f84dc8bbc8174d842
chris <xychen213@qq.com>
2022-07-04 08:59:11 +0800

    update

---
+++ edu/src/main/java/com/atguigu/app/DIM.java
@@ -0,0 +1,14 @@
+package com.atguigu.app;
+
+/**
+ * @ClassName DIM
+ * @Author Chris
+ * @Description TODO
+ * @Date 2022/7/4 8:57
+ **/
+
+public class DIM {
+	public static void main(String[] args) {
+		System.out.println(1);
+	}
+}
