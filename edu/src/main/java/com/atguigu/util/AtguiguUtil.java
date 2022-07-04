9d91b7a6b3bfb34dcd5ac0eff20a8acd45bbd680
chris <xychen213@qq.com>
2022-07-04 09:02:01 +0800

    update

---
+++ edu/src/main/java/com/atguigu/util/AtguiguUtil.java
@@ -0,0 +1,14 @@
+package com.atguigu.util;
+
+/**
+ * @ClassName AtguiguUtil
+ * @Author Chris
+ * @Description TODO
+ * @Date 2022/7/4 9:00
+ **/
+
+public class AtguiguUtil {
+	public static void main(String[] args) {
+		System.out.println(1);
+	}
+}
