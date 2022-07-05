5df14912a5139b948cf006ab1b473547bae63208
lz <905769775@qq.com>
2022-07-05 19:25:54 +0800

    dwdreview

--- edu/src/main/java/com/atguigu/app/dwd/db/DwdInteractionReview.java
+++ edu/src/main/java/com/atguigu/app/dwd/db/DwdInteractionReview.java
@@ -13,7 +13,7 @@ public class DwdInteractionReview extends BaseSQLApp {
         new DwdInteractionReview().init(
             2015,
             2,
-            "DwdInteractionComment",
+            "DwdInteractionReview",
             10
         );
     }
@@ -21,37 +21,37 @@ public class DwdInteractionReview extends BaseSQLApp {
     @Override
     protected void handle(StreamExecutionEnvironment env, StreamTableEnvironment tEnv) {
         //读取ods_db数据
-        readOdsDb(tEnv, "DwdInteractionComment");
+        readOdsDb(tEnv, "DwdInteractionReview");
 
         //过滤出课程评价表
-        Table commentInfo = tEnv.sqlQuery("select " +
+        Table reviewInfo = tEnv.sqlQuery("select " +
                                             "data['id'] id,  " +
                                             "data['user_id'] user_id,  " +
-                                            "data['chapter_id'] chapter_id,  " +
                                             "data['course_id'] course_id,  " +
-                                            "data['comment_txt'] comment_txt, " +
+                                            "data['review_txt'] review_txt, " +
+                                            "data['review_stars'] review_stars,  " +
                                             "date_format(data['create_time'],'yyyy-MM-dd') date_id,  " +
                                             "data['create_time'] create_time,  " +
                                             "cast(ts as string) ts  " +
                                             "from ods_db " +
                                             "where `database`='gmall' " +
-                                            "and `table`='comment_info' " +
+                                            "and `table`='review_info' " +
                                             "and `type`='insert' " +
                                             "and data['deleted']='0' " );
 
         // 结果写入到Kafka中
-        tEnv.executeSql("create table dwd_interaction_comment(  " +
+        tEnv.executeSql("create table dwd_interaction_review(  " +
                             "id string,  " +
                             "user_id string,  " +
-                            "chapter_id  string,  " +
                             "course_id string,  " +
-                            "comment_txt string,  " +
+                            "review_txt string,  " +
+                            "review_stars  string,  " +
                             "data_id string,  " +
                             "create_time string,  " +
                             "ts string  " +
                             ")" + SQLUtil.getKafkaSinkDDL(Constant.TOPIC_DWD_INTERACTION_REVIEW));
         
-        commentInfo.executeInsert("dwd_interaction_comment");
+        reviewInfo.executeInsert("dwd_interaction_review");
 
     }
 }
