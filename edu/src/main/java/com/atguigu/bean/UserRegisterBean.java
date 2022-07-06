27ec2a5e3d32596d0e3f037e22e38969154b24cd
chris <xychen213@qq.com>
2022-07-06 09:09:31 +0800

    update

---
+++ edu/src/main/java/com/atguigu/bean/UserRegisterBean.java
@@ -0,0 +1,19 @@
+package com.atguigu.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegisterBean {
+    // 窗口起始时间
+    String stt;
+    // 窗口终止时间
+    String edt;
+    // 注册用户数
+    Long registerCt;
+    // 时间戳
+    Long ts;
+}
