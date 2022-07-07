93364da6274665c04c56c8af303f0833cae59ba1
atguigu <atguigu.qq.com>
2022-07-07 17:28:58 +0800

    上传考试域dwd层及dws层demo,bean的修改

--- edu/src/main/java/com/atguigu/bean/TestQuestionAnswerBean.java
+++ edu/src/main/java/com/atguigu/bean/TestQuestionAnswerBean.java
@@ -4,10 +4,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
